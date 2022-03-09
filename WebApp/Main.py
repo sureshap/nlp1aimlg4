f2f6a2d2ffe4aecdc20badf52be7fd0a4efc19d0
sureshap <81589808+sureshap@users.noreply.github.com>
2022-03-09 18:59:00 +0530

    Add files via upload

--- WebApp/Main.py
+++ WebApp/Main.py
@@ -17,11 +17,7 @@ view(Model())
 
 import time
 
-with st.empty():
-     for seconds in range(60):
-         st.write(f"⏳ {seconds} seconds have passed")
-         time.sleep(1)
-     st.write("✔️ 1 minute over!")
+
 	 
 	 
 	 
