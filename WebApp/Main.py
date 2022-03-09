e629c76853104d53630c6e0dabbb0eec52f39308
sureshap <81589808+sureshap@users.noreply.github.com>
2022-03-09 16:20:30 +0530

    Add files via upload

---
+++ WebApp/Main.py
@@ -0,0 +1,12 @@
+##############################################
+# A simple model/view template for Streamlit #
+##############################################
+import streamlit as st
+################ Model ################
+class Model:
+   caption = "This is some text"
+################ View  ################
+def view(model):
+   st.write(model.caption)
+################ Start  ################
+view(Model())
