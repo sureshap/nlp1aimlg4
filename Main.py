e46021bdf0d0c81bf2025c9ceaf19d7c24d6d645
sureshap <81589808+sureshap@users.noreply.github.com>
2022-03-09 18:57:38 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,42 @@
+##############################################
+# A simple model/view template for Streamlit #
+##############################################
+import streamlit as st
+################ Model ################
+class Model:
+   caption = "Hello World"
+   Heading1 = "Automatic Ticket Assignment Tool"
+   Heading2 = "FixITApp"
+################ View  ################
+def view(model):
+   st.write(model.caption)
+################ Start  ################
+view(Model())
+
+
+
+import time
+
+with st.empty():
+     for seconds in range(60):
+         st.write(f"⏳ {seconds} seconds have passed")
+         time.sleep(1)
+     st.write("✔️ 1 minute over!")
+	 
+	 
+	 
+placeholder = st.empty()
+
+# Replace the placeholder with some text:
+placeholder.text("Hello")
+
+# Replace the text with a chart:
+placeholder.line_chart({"data": [1, 5, 2, 6]})
+
+# Replace the chart with several elements:
+with placeholder.container():
+     st.write("This is one element")
+     st.write("This is another")
+
+# Clear all those elements:
+#placeholder.empty()
