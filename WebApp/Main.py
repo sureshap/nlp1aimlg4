e98a8b7956953d6db495e10d24858deea8ae6ae6
sureshap <81589808+sureshap@users.noreply.github.com>
2022-03-09 18:19:03 +0530

    Add files via upload

--- WebApp/Main.py
+++ WebApp/Main.py
@@ -5,8 +5,22 @@ import streamlit as st
 ################ Model ################
 class Model:
    caption = "Hello World"
+   Heading1 = "Automatic Ticket Assignment Tool"
+   Heading2 = "FixITApp"
 ################ View  ################
 def view(model):
    st.write(model.caption)
 ################ Start  ################
 view(Model())
+
+
+
+st.line_chart({"data": [1, 5, 2, 6, 2, 1]})
+
+with st.expander("See explanation"):
+     st.write("""
+         The chart above shows some numbers I picked for you.
+         I rolled actual dice for these, so they're *guaranteed* to
+         be random.
+     """)
+     st.image("https://static.streamlit.io/examples/dice.jpg")
